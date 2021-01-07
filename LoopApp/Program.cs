d0ee7fc8ba57fd6b45e019c44325a0e3b654fe5c
Administrator <Administrator@DESKTOP-D3829DM>
2021-01-06 21:07:21 -0500

    Add project files.

---
+++ LoopApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Hello this is Brandon i=" + i);
+                if (i == 9)
+                    break; // break from the loop
+            }
+        }
+    }
+}
---
+++ WhileLoopApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while(counter < 1002)
+            {
+                Console.WriteLine($"Hello World! The counter is:" + counter);
+                counter = counter + 1;
+
+                if (counter == 1001)
+                    break;
+            }
+                
+
+            
+        }
+    }
+}
