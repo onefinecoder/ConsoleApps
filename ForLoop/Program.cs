c435ba3871afa38035fa7ab99d5f602d1e42b1da
summertimebrandon <summertimebrandon@outlook.com>
2021-02-13 17:43:13 -0500

    Loops

---
+++ ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+            }
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (true)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+        }
+    }
+}
