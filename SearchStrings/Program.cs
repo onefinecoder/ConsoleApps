65950924ad6c5d800ff4a8492ad8cb8df0a1cba4
summertimebrandon <summertimebrandon@outlook.com>
2021-02-12 21:33:10 -0500

    Math

---
+++ SearchStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string songLyrics = "You say goodbye, and I say hello";
+
+            var result = songLyrics.StartsWith("You");
+            Console.WriteLine(result);
+
+
+            var resultContains = songLyrics.Contains("say");
+            Console.WriteLine(resultContains);
+
+            var resultEndsWith = songLyrics.EndsWith("hello");
+            Console.WriteLine(resultEndsWith);
+
+            //Console.WriteLine(songLyrics.Contains("greetings"));
+
+        }
+    }
+}
